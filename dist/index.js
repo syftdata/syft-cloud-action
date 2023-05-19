e23127430cb6a5405377f445cfb585fc5a63cce0
Nageswara Mannem <nageswara@syftdata.com>
2023-05-18 22:38:04 -0700

    core.info

--- dist/index.js
+++ dist/index.js
@@ -41,9 +41,11 @@ async function setupPuppeteer() {
   //   "/etc/apt/sources.list.d/google.list",
   //   "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main"
   // );
-  await exec.exec(
-    `sudo sh -c 'echo \"deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main\" >> /etc/apt/sources.list.d/google.list'`
-  );
+  await exec.exec("sudo", [
+    "sh",
+    "-c",
+    "'echo \"deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main\" >> /etc/apt/sources.list.d/google.list'",
+  ]);
   await exec.exec(`sudo apt-get update`);
   await exec.exec(
     `sudo apt-get install -y google-chrome-stable --no-install-recommends`
